75ab336057f9d52a98ba0ddfc4537b22e40e1bd5
CarlosRaloy <cgarcia@raloy.com.mx>
2025-06-09 17:17:00 -0600

    Startr the first step

---
+++ aplications/users/models.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+"""
+Areas
+Levels
+"""
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    level = models.SmallIntegerField(default=0)
+    area = models.CharField(max_length=5,blank=True,null=True, default='NA')
+    theme = models.CharField(max_length=15, default='default')
+    picture = models.ImageField(upload_to='user/pictures', blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.username
